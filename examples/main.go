de61448cba25b4e949590af2084b00c4aba926fe
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-07-09 08:20:47 +0100

    update example

---
+++ examples/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mmatongo/chew"
+)
+
+func main() {
+	urls := []string{
+		"https://example.com",
+	}
+
+	// The context is optional
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	chunks, err := chew.Process(urls, ctx)
+	if err != nil {
+		if err == context.DeadlineExceeded {
+			log.Println("Operation timed out")
+		} else {
+			log.Printf("Error processing URLs: %v", err)
+		}
+		return
+	}
+
+	for _, chunk := range chunks {
+		fmt.Printf("Source: %s\nContent: %s\n\n", chunk.Source, chunk.Content)
+	}
+}
