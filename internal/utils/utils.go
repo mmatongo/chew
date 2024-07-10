d631e5ee64061006510bcdd1d636f3dec8ac6903
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-07-10 23:11:35 +0100

    some helper functions

---
+++ internal/utils/utils.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+func GetFileExtensionFromUrl(rawUrl string) (string, error) {
+	u, err := url.Parse(rawUrl)
+	if err != nil {
+		return "", err
+	}
+	pos := strings.LastIndex(u.Path, ".")
+	if pos == -1 {
+		return "", errors.New("couldn't find a period to indicate a file extension")
+	}
+	return u.Path[pos:len(u.Path)], nil
+}
