1f1b46d3778cce4ddca56b45f9c449c162ff2581
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-07-17 15:22:38 +0100

    enhance utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"archive/zip"
+	"encoding/xml"
 	"errors"
+	"io"
 	"net/url"
 	"strings"
 )
@@ -16,4 +19,48 @@ func GetFileExtensionFromUrl(rawUrl string) (string, error) {
 		return "", errors.New("couldn't find a period to indicate a file extension")
 	}
 	return u.Path[pos:len(u.Path)], nil
+}
+
+func ExtractTextFromXML(file *zip.File) ([]string, error) {
+	fileReader, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer fileReader.Close()
+
+	decoder := xml.NewDecoder(fileReader)
+	var contents []string
+	var currentParagraph strings.Builder
+	inParagraph := false
+
+	for {
+		token, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		switch element := token.(type) {
+		case xml.StartElement:
+			if element.Name.Local == "p" {
+				inParagraph = true
+				currentParagraph.Reset()
+			}
+		case xml.EndElement:
+			if element.Name.Local == "p" {
+				inParagraph = false
+				if trimmed := strings.TrimSpace(currentParagraph.String()); trimmed != "" {
+					contents = append(contents, trimmed)
+				}
+			}
+		case xml.CharData:
+			if inParagraph {
+				currentParagraph.Write(element)
+			}
+		}
+	}
+
+	return contents, nil
 }
