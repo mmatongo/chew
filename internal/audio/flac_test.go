0b413c70cd4a395fbb072faa707ef85312ede11f
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-08-26 23:13:47 +0100

    audio restructure

---
+++ internal/audio/flac_test.go
@@ -0,0 +1,55 @@
+package audio
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_flacProcessor_process(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		p       *flacProcessor
+		args    args
+		want    *audioInfo
+		wantErr bool
+	}{
+		{
+			name: "success",
+			p:    &flacProcessor{},
+			args: args{
+				filename: getRootPath() + "/testdata/audio/test.flac",
+			},
+			want: &audioInfo{
+				sampleRate:  96000,
+				numChannels: 2,
+				format:      "FLAC",
+			},
+			wantErr: false,
+		},
+		{
+			name: "file not found",
+			p:    &flacProcessor{},
+			args: args{
+				filename: getRootPath() + "/testdata/audio/test_new.flac",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &flacProcessor{}
+			got, err := p.process(tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("flacProcessor.process() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("flacProcessor.process() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ internal/audio/mp3_test.go
@@ -0,0 +1,64 @@
+package audio
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mp3Processor_process(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		p       *mp3Processor
+		args    args
+		want    *audioInfo
+		wantErr bool
+	}{
+		{
+			name: "success",
+			p:    &mp3Processor{},
+			args: args{
+				filename: getRootPath() + "/testdata/audio/test.mp3",
+			},
+			want: &audioInfo{
+				sampleRate:  44100,
+				numChannels: 2,
+				format:      "MP3",
+			},
+			wantErr: false,
+		},
+		{
+			name: "file not found",
+			p:    &mp3Processor{},
+			args: args{
+				filename: getRootPath() + "/testdata/audio/test_new.mp3",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid MP3 file",
+			p:    &mp3Processor{},
+			args: args{
+				filename: getRootPath() + "/testdata/audio/test.flac",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &mp3Processor{}
+			got, err := p.process(tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("mp3Processor.process() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mp3Processor.process() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
