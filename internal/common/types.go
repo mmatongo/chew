ce1f305e1efc72f08e8429e3c0417b57a69db547
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-09-04 20:21:13 +0100

    extend common types

--- internal/common/types.go
+++ internal/common/types.go
@@ -1,5 +1,11 @@
 package common
 
+import (
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
 type TranscribeOptions struct {
 	CredentialsJSON   []byte
 	Bucket            string
@@ -12,6 +18,17 @@ type TranscribeOptions struct {
 	WhisperAPIKey     string
 	WhisperModel      string
 	WhisperPrompt     string
+}
+
+type Config struct {
+	UserAgent       string
+	RetryLimit      int
+	RetryDelay      time.Duration
+	CrawlDelay      time.Duration
+	ProxyList       []string
+	RateLimit       rate.Limit
+	RateBurst       int
+	IgnoreRobotsTxt bool
 }
 
 type Chunk struct {
